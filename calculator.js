0f6954cfc6e5129647c50322bf8b279e920ca0da
Denis <denis.o.shapiro@gmail.com>
2019-05-18 23:45:41 -0400

    added functionality to all buttons except equals

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
+let displayValue = "";
+let displayArr = [];
+const operators = ["+", "−", "÷", "×"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const clearButton = $("#C");
+const display = $("h1");
+const backspaceButton = $("#backspace");
+const decimalButton = $("#decimal");
+const operatorButtons = $(".no-equal");
+
 function add(a, b){
     return a + b;
 }
@@ -18,13 +28,52 @@ function operate(operator, a, b){
     if (operator == "+"){
         return add(a, b);
     }
-    else if (operator == "-"){
+    else if (operator == "−"){
         return subtract(a, b);
     }
-    else if (operator == "*"){
+    else if (operator == "×"){
         return multiply(a, b);
     }
-    else if (operator == "/"){
+    else if (operator == "÷"){
         return divide(a, b);
     }
 }
+
+$("button.number").on("click", function(e){
+    if((display[0].textContent).length <= 17){
+        display[0].textContent += this.textContent;
+        displayArr.length == 0 ? displayArr.push(this.textContent) :  displayArr[displayArr.length -1]+= this.textContent;
+        console.log(displayArr);
+    }
+});
+
+clearButton.on("click", function(){
+    display[0].textContent = "";
+    decimalButton.prop('disabled', false);
+});
+
+backspaceButton.on("click", function(){
+    if((display[0].textContent).length > 0){
+        temp = display[0].textContent;
+        display[0].textContent = temp.substring(0, temp.length-1);
+    }
+});
+
+decimalButton.on("click", function(){
+    decimalButton.prop('disabled', true);
+    if((display[0].textContent).length <= 17){
+        display[0].textContent += this.textContent;
+        displayArr.length == 0 ? displayArr.push(this.textContent) : displayArr[displayArr.length -1]+= this.textContent;
+        console.log(displayArr);
+    }
+});
+
+operatorButtons.on("click", function(){
+    decimalButton.prop('disabled', false);
+    if((display[0].textContent).length <= 16){
+        display[0].textContent += this.textContent;
+        displayArr.push(this.textContent);
+        displayArr.push("");
+        console.log(displayArr);
+    }
+});
